f2b3303c431370ffda1724ec7bfd83a95cd1d87d
thanhxuancoffeeandmore@gmail.com <thanhxuancoffeeandmore@gmail.com>
2020-06-26 22:42:25 +0700

    a

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,54 @@
+
+<style type="text/css">
+<!--
+.style1 {font-size: 36px}
+.style3 {font-size: 36px; font-weight: bold; }
+-->
+</style>
+
+			<div class="container">
+		<div class="col-md-7 col-12">
+			<div class="section-article contactpage" style="  padding-left: 20px;">
+			  <p><span class="style3">Gi&#7899;i thi&#7879;u c&#7917;a h&agrave;ng</span></p>
+			  <form accept-charset="UTF-8" action="<?php echo base_url() ?>lien-he" id="contact" method="post">
+			    <div><strong>Đồ chơi trẻ con Dũng Toys - Thế giới đồ chơi của bé</strong> được thành lập năm 2020, là hệ thống bán lẻ chuyên các sản phẩm xe, đồ chơi dành cho bé. Ở Dũng Toys chúng tôi quan niệm xe, đồ chơi là người bạn đồng hành đáng tin cậy của các bé. Xe,đồ chơi cho bé phải thật sự tốt, mỗi chiếc xe, mỗi chiếc đồ chơi mang cá tính riêng, độc đáo, thân thiện và hơn hết phải mang lại sự hứng thú cho các bé khi chơi.</div>
+				    <div></div>
+				    <div>Các sản phẩm tại Dũng Toys có chất lượng cao cấp, được lựa chọn tỉ mỉ bởi các chuyên gia cơ khí nhằm mang đến cho các bé những trải nghiệm tuyệt vời nhất. </div>
+				    <div></div>
+				    <div>Tất cả sản phẩm bán tại Dũng Toys đều phải đạt các tiêu chuẩn về an toàn cho bé như CE, ISO, EN, SGS. Có chất lượng đạt chuẩn xuất khẩu vào các thị trường Châu Âu, Nhật, Hàn. </div>
+				    <div></div>
+				    <div>Với triết lý kinh doanh:&quot;Chất lượng tạo niềm tin&quot;, Dũng Toys luôn là địa chỉ tin cậy dành cho các bậc phụ huynh khi có ý định sắm cho con một chiếc xe tốt với giá trị sử dụng lâu dài.</div>
+				    <div></div>
+				    <div><strong>Dòng sản phẩm chính của Dũng Toys:</strong></div>
+				    <div>- Đồ chơi</div>
+				    <div>- Xe thăng bằng cho bé</div>
+				    <div>- Xe chòi chân</div>
+				    <div>- Xe Scooter</div>
+				    <div>- Phụ kiện xe và các loại đồ chơi vận động khác.</div>
+				    <div></div>
+				    <div><strong>Xem trực tiếp sản phẩm của Dũng Toys tại:</strong></div>
+				    <div>Hóc Môn - Thành Phố Hồ Chí Minh </div>
+				    <div><strong>Thông tin liên hệ:</strong></div>
+				    <div>Hotline: 1900 1208 </div>
+			      <div>Website: https://www.dochoitrecon.vn</div>
+				    <p style="color: black">&nbsp;</p>
+				    <div class="form-comment">
+			    <div class="row" style="padding-left: 14px;">
+			    <div class="col-md-4 col-12">
+			    <div class="form-group" style="width: 200px;">
+		      </form>
+		  </div>
+		</div>
+		<div class="col-md-4 col-12">
+		  <div class="f-contact" style="
+			padding-left: 32px;
+			"></div>
+
+	</div>
+	
+		   <div>
+		
+		      <div>
+		     
+		        <div></div>
+		        
